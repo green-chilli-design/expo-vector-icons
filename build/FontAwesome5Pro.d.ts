f6053c2b1a660dccf8e6802a3d06e040d2b642f4
Eden Le Comte <edenlc@gmail.com>
2023-07-10 13:14:47 +1200

    rebuild packages

--- build/FontAwesome5Pro.d.ts
+++ build/FontAwesome5Pro.d.ts
@@ -1,8 +1,8 @@
 export declare const FA5Style: {
-  regular: string;
-  light: string;
-  solid: string;
-  brand: string;
+    regular: string;
+    light: string;
+    solid: string;
+    brand: string;
 };
 declare const iconSet: any;
 export default iconSet;
